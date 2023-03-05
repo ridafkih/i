9e1690c9524a0f135bbdfa10345a8712588a1d4b
Rida <hello@rida.dev>
2023-03-04 23:28:35 -0700

    change location key to "url"

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -8,7 +8,7 @@ export default route({
   post: {
     schema: {
       request: v.object({
-        location: v.string().url().startsWith("https://maps.apple.com"),
+        url: v.string().url().startsWith("https://maps.apple.com"),
         city: v.string(),
         region: v.string(),
         longitude: v.string(),
@@ -20,7 +20,7 @@ export default route({
     },
     middleware: { pre: [logRequest, ensureAuthenticated] },
     async handler(context) {
-      const { location: url, city, region, longitude, latitude } = context.request.body;
+      const { url, city, region, longitude, latitude } = context.request.body;
       const { id } = await addLocation(url, city, region, parseFloat(longitude), parseFloat(latitude));
       
       return { body: { id } };
